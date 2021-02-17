12954c0dc2cb93c8c72eb3ad70e38340eb8ece61
Vinay Kumar <vny069@gmail.com>
2021-02-17 22:19:15 +0530

    intial commit

---
+++ src/OperatorDemo.java
@@ -0,0 +1,15 @@
+public class OperatorDemo {
+    public static void main(String[] args) {
+        int a=89;
+        int b=67;
+        int sum = a+b;
+        int diff = a-b;
+        int mul = a*b;
+        int divide = a/b;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(divide);
+    }
+}
