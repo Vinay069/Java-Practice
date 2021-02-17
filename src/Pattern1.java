c87d36eee54d17590f52b94bceb0c0ebe05e5657
Vinay Kumar <vny069@gmail.com>
2021-02-17 22:32:47 +0530

    added Pattern1.java

---
+++ src/Pattern1.java
@@ -0,0 +1,24 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n=6,num=1;
+            /*
+            1
+            1 2
+            1 2 3
+            1 2 3 4
+            1 2 3 4 5
+            1 2 3 4 5 6
+             */
+
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+//                System.out.print(num+" ");
+//                num++;
+                System.out.print((j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
